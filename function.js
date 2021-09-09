83d2fb414ca9f8aef0038a22c26898499d7cf173
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-09 23:11:19 +0600

    upload

---
+++ function.js
@@ -0,0 +1,24 @@
+function evenify(num) {
+       if(num % 2 == 0){
+        console.log(num, ':is even number')
+    }
+     else{
+          console.log(num*2 , ':is odd number')
+     }
+}
+
+
+nums = [5, 12, 89, 45, 18, 8];
+
+ for (i = 0; i< nums.length; i++){ 
+     const num =  nums[i];
+     evenify(num)
+ }
+ 
+
+
+ friends_age = [13, 17, 19, 20, 18];
+ for (let i = 0; i < friends_age.length; i++) {
+     const age = friends_age[i];
+     evenify(age)
+ }
