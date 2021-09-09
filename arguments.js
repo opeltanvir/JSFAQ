463ca21af510c98449270657822a1b2b86524ec7
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-10 02:48:57 +0600

    upload

---
+++ arguments.js
@@ -0,0 +1,13 @@
+
+ function addNumber(num1, num2) {
+     var sum = 0;
+     for (let i = 0; i< arguments.length; i++) {
+       const num = arguments[i];
+    sum = sum +sum ;
+        
+     }
+     return sum ;
+ }
+
+ var result = addNumber(3 ,5 ,15 ,);
+ console.log(result);
